88e0b18c836047552c044448e5f140e7b0dc8d03
Mohankrishnashrestha <shrestharajan0077@gmail.com>
2024-12-23 21:40:16 +0545

    add

--- new.js
+++ new.js
@@ -1,39 +1,39 @@
-// // // const names = ["mohan", "sangram", "bizen", "abhishek"];
-// // // console.log(names);
-// // // console.log(names[1]);
-// // // let i;
-// // // // for(i=0;i<names.length;i++){
-// // // //     console.log(names[i]);
-// // // // }
-// // // const array = document.getElementById("array");
-// // // names.forEach((element) => {
-// // //   const textnode = document.createTextNode(element);
-// // //   array.appendChild(textnode);
-// // //   console.log(element);
-// // // });
-// // function myFunction(){
-// //     const names =["mohan","rajan","krishna","sangram","bizen"];
-
-// //     names.forEach(element => {
-// //         const node=document.createElement("li");
-
-// //         const textnode = document.createTextNode(element);
-// //         node.appendChild(textnode);
-// //         document.getElementById("ul").appendChild(node);
-// //     });
-// // }
- 
-// const add = ["rajan","sajan","sangram"];
+// // const names = ["mohan", "sangram", "bizen", "abhishek"];
+// // console.log(names);
+// // console.log(names[1]);
+// // let i;
+// // // for(i=0;i<names.length;i++){
+// // //     console.log(names[i]);
+// // // }
+// // const array = document.getElementById("array");
+// // names.forEach((element) => {
+// //   const textnode = document.createTextNode(element);
+// //   array.appendChild(textnode);
+// //   console.log(element);
+// // });
 // function myFunction(){
-//     add.forEach(element => {
-//         const a=document.createElement("li");
+//     const names =["mohan","rajan","krishna","sangram","bizen"];
 
-//     const t= document.createTextNode(element);
-//     a.appendChild(t);  
-//     document.getElementById("ul").appendChild(a);
+//     names.forEach(element => {
+//         const node=document.createElement("li");
+
+//         const textnode = document.createTextNode(element);
+//         node.appendChild(textnode);
+//         document.getElementById("ul").appendChild(node);
+//     });
+// }
  
-//     });}
-// wap to show the data in html from js in table form
+const add = ["rajan","sajan","sangram"];
+function myFunction(){
+    add.forEach(element => {
+        const a=document.createElement("li");
 
+    const t= document.createTextNode(element);
+    a.appendChild(t);  
+    document.getElementById("ul").appendChild(a);
+ 
+    });}
+ 
+    const data  = []
 
     
